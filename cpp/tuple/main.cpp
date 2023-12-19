5941097fef92414f8f40259dd13c5ce62bab2cb4
Simón Gutiérrez Brida <simon.gutierrez.brida@gmail.com>
2023-12-19 14:32:14 +0800

    Changed tuple class name to suit naming conventions

--- cpp/tuple/main.cpp
+++ cpp/tuple/main.cpp
@@ -2,7 +2,7 @@
 #include <tuple_ints.hpp>
 
 int main(void) {
-    Tuple t1(1, 2);
+    tuple t1(1, 2);
     std::cout << "(" << t1.get_fst() << ", " << t1.get_snd() << ")" << std::endl;
     return 0; 
 }
