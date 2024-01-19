82b385d6cbfc6ae865ac06fbc011fa4e77667a6f
Simón Gutiérrez Brida <simon.gutierrez.brida@gmail.com>
2024-01-19 14:53:55 +0800

    Added comments

--- introduction/structs/palindrome/stack_char.h
+++ introduction/structs/palindrome/stack_char.h
@@ -9,10 +9,29 @@
 struct Stack_Char;
 typedef struct Stack_Char * stack_char;
 
+/*
+    Creates a new empty stack
+*/
 extern stack_char stack_char_new(void);
+
+/*
+    Returns `TRUE` iff the stack is empty
+*/
 extern boolean stack_char_is_empty(const stack_char const stack);
+
+/*
+    Pushes an element (a char) into the stack
+*/
 extern boolean stack_char_push(stack_char const stack, const int value);
+
+/*
+    Removes, and returns, the element at the top of a Non-empty stack
+*/
 extern int stack_char_pop(stack_char const stack);
+
+/*
+    Frees all memory allocated for a stack
+*/
 extern void stack_char_drop(stack_char stack);
 
 #endif
--- introduction/structs/palindrome/stack_int.h
+++ introduction/structs/palindrome/stack_int.h
@@ -9,10 +9,29 @@
 struct Stack_Int;
 typedef struct Stack_Int * stack_int;
 
+/*
+    Creates a new empty stack
+*/
 extern stack_int stack_int_new(void);
+
+/*
+    Returns `TRUE` iff the stack is empty
+*/
 extern boolean stack_int_is_empty(const stack_int const stack);
+
+/*
+    Pushes an element (a char) into the stack
+*/
 extern boolean stack_int_push(stack_int const stack, const int value);
+
+/*
+    Removes, and returns, the element at the top of a Non-empty stack
+*/
 extern int stack_int_pop(stack_int const stack);
+
+/*
+    Frees all memory allocated for a stack
+*/
 extern void stack_int_drop(stack_int stack);
 
 #endif
